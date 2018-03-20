de18566bba1693983b135ab3daff12c5b8811423
SebastianPegoraro <sebapego18@gmail.com>
2018-03-20 13:06:56 -0300

    Terminando Inscripcion, falta prueba

--- olimpiadas/guarda.php
+++ olimpiadas/guarda.php
@@ -5,21 +5,62 @@
   $jurisdiccion = $_REQUEST['jurisdiccion'];
   $deporte = $_REQUEST['deporte'];
   $combinacion = $_REQUEST['combinacion'];
+  $nomequipo = $_REQUEST['nomequipo'];
 
-  //Consulta el numero de equipos que ya existen de esa jurisdiccion
+  //Solo estos deportes tienen equipos limitados
   if ($deporte == 2 || $deporte == 4 || $deporte == 5 || $deporte == 5) {
-    $stmt = $dbh->prepare("SELECT especialidad.nombre, especialidad.id_especialidad FROM especialidad INNER JOIN combinacion ON combinacion.id_especialidad = especialidad.id_especialidad WHERE combinacion.id_deporte = 2 GROUP BY especialidad.nombre");
+    //Consulta el numero de equipos que ya existen de esa jurisdiccion
+    $stmt = $dbh->prepare("SELECT COUNT(DISTINCT persona.numequipo) FROM persona INNER JOIN inscripcion ON persona.id_persona = inscripcion.id_persona
+                            WHERE inscripcion.id_combinacion = ".$combinacion." AND persona.denominacionjur = ".$jurisdiccion." GROUP BY numequipo");
     $stmt->execute();
+    $lista = $stmt->fetchAll();
+    $numequipo = count($lista);
+
+    for ($i=0; $i < $jugadores ; $i++) {
+
+      $nombre = $_REQUEST['apellido'+$i] + $_REQUEST['nombre'+$i];
+      $nacimiento = $_REQUEST['nacimiento'+$i];
+      $dni = $_REQUEST['dni'+$i];
+      $pasbec = $_REQUEST['pasbec'+$i];
+      $abscripto = $_REQUEST['abscripto'+$i];
+      //Agregamos la persona
+      $stmt2 = $dbh->prepare("INSERT INTO persona(nombre, edad, dni, denominacionjur, numequipo, pasbec, abscripto)
+                              VALUES (".$nombre.",".$nacimiento.",".$dni.",".$jurisdiccion.",".$numequipo.",".$pasbec.",".$abscripto.") ");
+      $stmt2->execute();
+      //Buscamos el id de la persona que acabamos de agregar
+      $stmt3 = $dbh->prepare("SELECT id_persona FROM persona WHERE dni = ".$dni." AND numequipo = ".$numequipo);
+      $stmt3->execute();
+      $id_persona = $stmt3->fetchAll();
+      //Agregamos la combinacion para formar al equipo
+      $stmt4 = $dbh->prepare("INSERT INTO inscripcion(id_persona, id_combinacion, fecha_inscripcion, nomequipo)
+                              VALUES (".$id_persona.",".$combinacion.",".date("Y-m-d").",".$nomequipo.") ");
+      $stmt4->execute();
+    }
+    //Una vez terminado, redireccionamos a otra pagina
+    header("Location: guardado.html");
+  } else {
+    for ($i=0; $i < $jugadores ; $i++) {
+
+      $nombre = $_REQUEST['apellido'+$i] + $_REQUEST['nombre'+$i];
+      $nacimiento = $_REQUEST['nacimiento'+$i];
+      $dni = $_REQUEST['dni'+$i];
+      $pasbec = $_REQUEST['pasbec'+$i];
+      $abscripto = $_REQUEST['abscripto'+$i];
+      //Agregamos la persona
+      $stmt2 = $dbh->prepare("INSERT INTO persona(nombre, edad, dni, denominacionjur, pasbec, abscripto)
+                              VALUES (".$nombre.",".$nacimiento.",".$dni.",".$jurisdiccion.",".$pasbec.",".$abscripto.") ");
+      $stmt2->execute();
+      //Buscamos el id de la persona que acabamos de agregar
+      $stmt3 = $dbh->prepare("SELECT id_persona FROM persona WHERE dni = ".$dni." AND numequipo = ".$numequipo);
+      $stmt3->execute();
+      $id_persona = $stmt3->fetchAll();
+      //Agregamos la combinacion para formar al equipo
+      $stmt4 = $dbh->prepare("INSERT INTO inscripcion(id_persona, id_combinacion, fecha_inscripcion, nomequipo)
+                              VALUES (".$id_persona.",".$combinacion.",".date("Y-m-d").",".$nomequipo.") ");
+      $stmt4->execute();
+    }
+    //Una vez terminado, redireccionamos a otra pagina
+    header("Location: guardado.html");
   }
 
-  for ($i=0; $i < $jugadores ; $i++) {
-
-    $nombre = $_REQUEST['apellido'+$i] + $_REQUEST['nombre'+$i];
-    $nacimiento = $_REQUEST['nacimiento'+$i];
-    $dni = $_REQUEST['dni'+$i];
-    $pasbec = $_REQUEST['pasbec'+$i];
-
-    $stmt = $dbh->prepare("INSERT INTO persona(nombre, edad, dni, denominacionjur, numequipo, pasbec) VALUES (".$nombre.",".$nacimiento.",".$dni.",".$jurisdiccion.",[value-5],".$pasbec.") ");
-    $stmt->execute();
-  }
 ?>
