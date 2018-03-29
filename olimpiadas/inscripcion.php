8855a82f053043cb111bb246e08d48c164dd8cac
martinebel <36831742+martinebel@users.noreply.github.com>
2018-03-29 01:48:08 -0300

    calendario
    agregado script para control de calendario en la fecha de nacimiento. prestar atencion al ToDo!

--- olimpiadas/inscripcion.php
+++ olimpiadas/inscripcion.php
@@ -119,5 +119,14 @@
     <script src="../js/ajax.js"></script>
 
     <script src="../js/inscripcion.js"></script>
+	  <!-- ToDo: descargar el JS y CSS de JQueryUI y usarlos localmente -->
+	  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	  <script src="../js/locales.js"></script>
+ <script>
+  $( function() {
+    $( ".nacimiento" ).datepicker({ dateFormat: 'dd-mm-yy',firstDay: 7 });
+  } );
+  </script>
   </body>
 </html>
